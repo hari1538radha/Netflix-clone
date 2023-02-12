f6abff5307f7d6a18202ddc64a5f9d3032f936c3
hari1538radha <hari.r2020ece@sece.ac.in>
2023-02-12 16:25:04 +0530

    fix : added the environmental varialbles for vercel

--- src/Store/Slice/TrendingSlice.js
+++ src/Store/Slice/TrendingSlice.js
@@ -1,15 +1,17 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-
-export const MoviedbInfo = createAsyncThunk("trendingmoviedata", async (data) => {
-  return fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=&language=en-US&page=52`,
-    {
-      method: "GET",
-      headers: {},
-    }
-  ).then((response) => response.json());
-});
+export const MoviedbInfo = createAsyncThunk(
+  "trendingmoviedata",
+  async (data) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=52`,
+      {
+        method: "GET",
+        headers: {},
+      }
+    ).then((response) => response.json());
+  }
+);
 const MovieData = createSlice({
   name: "trendingmoviedatas",
   initialState: {
